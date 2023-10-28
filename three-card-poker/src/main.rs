9b16932603ad20ed9affa83bfc0da817eb3c062b
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2023-10-27 16:46:06 -0800

    feat: example console output & playing cards

--- three-card-poker/src/main.rs
+++ three-card-poker/src/main.rs
@@ -1,3 +1,23 @@
+// main.rs
+// Solomon Himelbloom
+// 2023-10-27
+//
+// For CS 411 Fall 2023
+
 fn main() {
-    println!("Three Card Poker");
+    let gui = "🃏";
+    let bet = 1.00;
+    let total_return = 0.00;
+
+    println!(">> Three Card Poker {}", gui);
+
+    // https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
+    for suit in ['♠', '♥', '♦', '♣'].iter() {
+        print!("{} ", suit);
+    }
+
+    println!(
+        "\n[ Current Bet: ${} | Total Return: ${} ]",
+        bet, total_return
+    );
 }
