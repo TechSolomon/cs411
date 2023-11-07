f5601b89d92fa9861987dde0d6cdc21cd876801f
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2023-11-06 23:45:28 -0900

    feat: total returns as CLI table

--- three-card-poker/src/main.rs
+++ three-card-poker/src/main.rs
@@ -49,13 +49,42 @@ fn payout_table() {
     // Once you have the probabilities, multiply by the payout and then sum for the total return.
 
     const STRAIGHT_FLUST: u32 = 100;
-    const THREE_OF_A_KIND: u32 = 50;
+    const THREE_OF_A_KIND: u32 = 30;
     const STRAIGHT: u32 = 15;
     const FLUSH: u32 = 5;
     const PAIR: u32 = 1;
     const HIGH_CARD: u32 = 0;
 
     let mut table = Table::new();
+
+    // Frequency
+
+    let first = 48;
+    let second = 52;
+    let third = 720;
+    let fourth = 1096;
+    let fifth = 3744;
+    let sixth = 16440;
+
+    // Probability
+
+    let permutations = 22100;
+
+    let prob1 = first as f32 / permutations as f32;
+    let prob2 = second as f32 / permutations as f32;
+    let prob3 = third as f32 / permutations as f32;
+    let prob4 = fourth as f32 / permutations as f32;
+    let prob5 = fifth as f32 / permutations as f32;
+    let prob6 = sixth as f32 / permutations as f32;
+
+    // Returns
+
+    let ret1 = prob1 * STRAIGHT_FLUST as f32;
+    let ret2 = prob2 * THREE_OF_A_KIND as f32;
+    let ret3 = prob3 * STRAIGHT as f32;
+    let ret4 = prob4 * FLUSH as f32;
+    let ret5 = prob5 * PAIR as f32;
+    let ret6 = prob6 * HIGH_CARD as f32;
 
     table.add_row(row![
         "HAND",
@@ -68,39 +97,50 @@ fn payout_table() {
     table.add_row(row![
         "Straight Flush",
         "3 suited in sequence",
-        "--",
-        "--",
+        first,
+        prob1,
         STRAIGHT_FLUST,
-        "--"
+        ret1
     ]);
     table.add_row(row![
         "Three of a Kind",
         "3 of the same rank",
-        "--",
-        "--",
+        second,
+        prob2,
         THREE_OF_A_KIND,
-        "--"
+        ret2
     ]);
     table.add_row(row![
         "Straight",
         "3 in sequence (includes AKQ)",
-        "--",
-        "--",
+        third,
+        prob3,
         STRAIGHT,
-        "--"
+        ret3
     ]);
-    table.add_row(row!["Flush", "3 suited", "--", "--", FLUSH, "--"]);
-    table.add_row(row!["Pair", "2 of the same rank", "--", "--", PAIR, "--"]);
+    table.add_row(row!["Flush", "3 suited", fourth, prob4, FLUSH, ret4]);
+    table.add_row(row!["Pair", "2 of the same rank", fifth, prob5, PAIR, ret5]);
     table.add_row(row![
         "High Card",
         "None of the above",
-        "--",
-        "--",
+        sixth,
+        prob6,
         HIGH_CARD,
-        "--"
+        ret6
     ]);
 
     table.printstd();
+
+    let bet = 1.00; // Assume a player makes a $1 bet and will get a payout for a pair or higher.
+
+    let total_return = ret1 + ret2 + ret3 + ret4 + ret5 + ret6;
+
+    let total_return = (total_return * 100.0).round() / 100.0;
+
+    println!(
+        "Current Bet: ${}                                                                 Total Return: ${}",
+        bet, total_return
+    );
 }
 
 fn main() {
@@ -108,16 +148,7 @@ fn main() {
 
     println!(">> Three Card Poker {}", gui);
 
-    let bet = 1.00; // Assume a player makes a $1 bet and will get a payout for a pair or higher.
-
-    let total_return = 0.00;
-
     generate_hand();
 
     payout_table();
-
-    println!(
-        "Current Bet: ${}                                                               Total Return: ${}",
-        bet, total_return
-    );
 }
